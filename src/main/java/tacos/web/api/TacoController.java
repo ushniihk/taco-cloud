170217ab94cad089f5d0edb7aa53d3380c0cdaa6
ushnii <ushniihk@yandex.ru>
2023-01-13 22:08:33 +0300

    1

---
+++ src/main/java/tacos/web/api/TacoController.java
@@ -0,0 +1,27 @@
+package tacos.web.api;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+@RestController
+@RequestMapping(path = "/api/tacos", produces = "application/json")
+@CrossOrigin(origins = "http://tacocloud:8080")
+public class TacoController {
+    private TacoRepository tacoRepo;
+
+    public TacoController(TacoRepository tacoRepo) {
+        this.tacoRepo = tacoRepo;
+    }
+
+    @GetMapping(params = "recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        return tacoRepo.findAll(page).getContent();
+    }
+}
